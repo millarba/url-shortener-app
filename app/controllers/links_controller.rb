ace68490cb6595298f473dcb825cb1cb8ef068be
millarba <millarba@gmail.com>
2017-02-12 09:02:02 -0600

    working product, mostly

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,15 +5,53 @@ class LinksController < ApplicationController
   end
 
   def new
-
+    @link = Link.new
   end
 
   def create
     @link = Link.create(
-                        user_id: current_user,
+                        user_id: 1,
                         slug: params[:slug],
                         target_url: params[:target_url]
                         )
+    @link.standardize_target_url!
+
+    redirect_to '/links'
   end
+
+  def show
+    @link = Link.find_by(:id => params[:id], :user_id => 1)
+
+    unless @link
+      flash[:warning] = "Link not found"
+      redirect_to "/index"
+    end
+  end
+
+  def edit
+    @link = Link.find_by(:id => params[:id], :user_id => 1)
+  end
+
+  def update
+    @link = Link.find_by(:id => params[:id], :user_id => 1)
+
+        if @link && @link.update(:slug => params[:slug], :target_url => params[:target_url])
+          @link.standardize_target_url!
+          flash[:success] = "Link created successfully"
+          redirect_to "/links/#{params[:id]}"
+        else
+          render 'edit'
+        end
+  end
+
+  def destroy
+      @link = Link.find_by(:id => params[:id], :user_id => 1)
+
+      if @link && @link.destroy
+        redirect_to "/links"
+      else
+        redirect_to "/links/#{params[:id]}"
+      end
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,13 @@ Rails.application.routes.draw do
  get '/links' => 'links#index'
  get '/links/new' => 'links#new'
  post '/links' => 'links#create'
+ get '/links/:id' => 'links#show'
+ get '/links/:id/edit' => 'links#edit'
+ patch '/links' => 'links#update'
+ delete '/links/:id' => 'links#destroy'
+
+ get '/:slug' => 'visits#create'
+
+ get '/visits/new' => 'visits#new'
 
 end
